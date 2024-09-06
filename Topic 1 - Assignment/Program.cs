7d7f4d4584d7b9039870c448380ef9f5e1fce91e
339723967 <339723967@2260I-L29-010>
2024-09-06 08:59:09 -0400

    Add project files.

---
+++ Topic 1 - Assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Andre
+Console.WriteLine("Hello there,this is Andre");
+Console.WriteLine("");
+Console.WriteLine("We use comments because they can be a place holder for info that way we dont forget it on a later date.");
+Console.WriteLine("");
+Console.WriteLine("  A    M   M");
+Console.WriteLine(" A A   MM MM");
+Console.WriteLine("A   A  MM MM");
+Console.WriteLine("AAAAA  M M M");
+Console.WriteLine("A   A  M   M");
+Console.WriteLine("A   A  M   M");
+Console.WriteLine("A   A  M   M");
